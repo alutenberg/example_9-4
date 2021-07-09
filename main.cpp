f1e55953f753fa8f3700ff14c4b4961f75202737
armBookCodeExamples <elpablogomez@gmail.com>
2021-07-09 13:32:24 -0300

    initial commits

--- main.cpp
+++ main.cpp
@@ -1,10 +1,13 @@
-#include "mbed.h"
+//=====[Libraries]=============================================================
 
-// main() runs in its own thread in the OS
+#include "smart_home_system.h"
+
+//=====[Main function, the program entry point after power on or reset]========
+
 int main()
 {
+    smartHomeSystemInit();
     while (true) {
-
+        smartHomeSystemUpdate();
     }
 }
-
