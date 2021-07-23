cc89b61de0c4b0ca39f1c55090dc95d406725afa
armBookCodeExamples <elpablogomez@gmail.com>
2021-07-23 01:18:54 -0300

    initial commits

--- modules/display/display.cpp
+++ modules/display/display.cpp
@@ -440,7 +440,6 @@ static void displayPinWrite( uint8_t pinName, int value )
                 case DISPLAY_PIN_RW: break; 
                 default: break;
             }
-        break;
 
         case DISPLAY_CONNECTION_I2C_PCF8574_IO_EXPANDER:
            if ( value ) {
@@ -477,10 +476,9 @@ static void displayPinWrite( uint8_t pinName, int value )
             if ( pcf8574.displayPin_D4 ) pcf8574.data |= 0b00010000; 
             if ( pcf8574.displayPin_D5 ) pcf8574.data |= 0b00100000; 
             if ( pcf8574.displayPin_D6 ) pcf8574.data |= 0b01000000; 
-            if ( pcf8574.displayPin_D7 ) pcf8574.data |= 0b10000000;                                     
-            I2C_PCF8574.write( pcf8574.address, &pcf8574.data, 1);    
-        break; 
-                        	
+            if ( pcf8574.displayPin_D7 ) pcf8574.data |= 0b10000000; 
+            I2C_PCF8574.write( pcf8574.address, &pcf8574.data, 1);
+
         case DISPLAY_CONNECTION_SPI:
         break;
     }
